125161329b106483f8658f42545f2ad055ffa339
Crayon-Cube <crayoncubeone@gmail.com>
2023-04-27 12:44:10 +0530

    Next image,styles,usernames

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,25 @@
 import { type Config } from "tailwindcss";
 
 export default {
+  
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 } satisfies Config;
+
+// tailwind.config.ts
+
+// import type { Config } from 'tailwindcss';
+
+// const config: Config = {
+//   mode: 'jit',
+//   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// export default config;
